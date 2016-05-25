470a57705a7a4eace11aa930555519f79e81ac9f
dakinshin <dakinshin@corp.finam.ru>
2016-05-25 16:27:39 +0300

    add project

---
+++ DriverStorageCleaner/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+
+namespace DiskCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = IsUserAdmin();
+
+            if (isAdmin)
+            {
+                List<string> drvToRemove = GetOemDrivers();
+
+                for (int i = 0; i < drvToRemove.Count; i++)
+                {
+                    string drv = drvToRemove[i];
+                    Console.WriteLine("[ " + (i + 1) + "/" + drvToRemove.Count + " ] Remove driver ... " + drv);
+
+                    RemoveDrv(drv);
+                }
+            }
+            else
+                Console.WriteLine("[err] You must be admin!");
+
+            Console.WriteLine("[inf] Press any key to continue ...");
+
+            Console.ReadKey();
+        }
+
+        private static void RemoveDrv(string drv)
+        {
+            ProcessStartInfo procStart = new ProcessStartInfo("pnputil.exe", "-d " + drv)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                //RedirectStandardOutput = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process proc = Process.Start(procStart);
+
+            proc.WaitForExit();
+
+            //Console.WriteLine("Remove driver end");
+        }
+
+        private static List<string> GetOemDrivers()
+        {
+            List<string> result = new List<string>();
+
+            Console.WriteLine("Collect third party drivers ...");
+
+            MemoryStream output = new MemoryStream();
+
+            ProcessStartInfo procStart = new ProcessStartInfo("pnputil.exe", "-e")
+            {
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process util = Process.Start(procStart);
+
+            while (!util.StandardOutput.EndOfStream)
+            {
+                string data = util.StandardOutput.ReadLine();
+                if (data.IndexOf(".inf") > -1)
+                    result.Add(data);
+            }
+
+            util.WaitForExit();
+            Console.WriteLine("Collect third party drivers end, found drivers: " + result.Count);
+
+            return result;
+        }
+
+        public static bool IsUserAdmin()
+        {
+            bool isAdmin;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                isAdmin = false;
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+    }
+}
---
+++ DriverStorageCleaner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DiskCleaner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiskCleaner")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ded8adf6-0117-4658-9f32-905907f9211b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
